7a269dacb7cd3200754a7f5866f9e9622cfb5213
dariancmagee <dariancmagee@gmail.com>
2022-04-12 20:29:02 -0400

    first commit

---
+++ src/components/Translator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import rot13 from 'rot-thirteen';
+
+class Translator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: props.initialText || ''
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Translator</h1>
+                <input type="text" onChange={(event) => {
+                    console.log(event.target.value);
+                    this._updateText(event.target.value);
+                }}/>
+                <p>{this.state.text}</p>
+                <p>{(this.state.translated)}</p>
+            </div>
+        );
+    }
+
+    _updateText = (newText) => {
+        this.setState({
+            text: newText,
+            translated: rot13(newText)
+        }, () => {
+            console.log(`Text updated to: ${newText}`);
+        })
+    }
+}
+
+export default Translator;
